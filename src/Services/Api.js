806238a79ca41a89be6749717b82a8bf5e97fa09
Douglas <douglas.aguiar410@gmail.com>
2022-05-26 15:53:15 -0300

    feat: creating endpoint to fetch data

---
+++ src/Services/Api.js
@@ -0,0 +1,18 @@
+import Axios from 'axios';
+
+const Api = Axios.create({
+  baseURL: 'http://localhost:8983/solr/mexico'
+});
+
+// eslint-disable-next-line
+export default {
+  getSearch: async (term) => {
+    if(term === undefined) {
+      let response = await Api.get('/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=*:*&fl=*&wt=json');
+      return response;
+    }else {
+      let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=${term}&fl=*&wt=json`);
+      return response;
+    } 
+  }
+};
