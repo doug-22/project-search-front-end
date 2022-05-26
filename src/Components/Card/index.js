45629eaa2c1a6d765d9b01c051437eab93a415d9
Douglas <douglas.aguiar410@gmail.com>
2022-05-26 16:00:17 -0300

    refactor: getting data from api

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,42 +3,41 @@ import { BsBack } from 'react-icons/bs';
 
 import './styles.css';
 
-function Card() {
+function Card({subject, secretary, file, description}) {
     
-  let description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   if (description.length > 200) {
 		description = description.substring(0, 200) + '...';
-	}  
+	}
+
+  const newFileName = file.slice(0, -5);
 
 
   return (
-      <div className='container-card'>
-          <div className='content-card-head'>
-              <div>
-                <div className='content-title'>
-                    <p style={{fontWeight: 'bold'}}>Assunto:</p>
-                    <p>Teste</p>
-                </div>
-                <div className='content-title'>
-                    <p style={{fontWeight: 'bold'}}>Secretário:</p>
-                    <p>Douglas</p>
-                </div>
-                <div className='content-title'>
-                    <p style={{fontWeight: 'bold'}}>Arquivo:</p>
-                    <p>Teste</p>
-                </div>
-              </div>
-              <button>
-                  <p>Abrir</p>
-                  <BsBack />
-              </button>
+    <div className='container-card'>
+      <div className='content-card-head'>
+        <div>
+          <div className='content-title'>
+            <p style={{fontWeight: 'bold'}}>Assunto:</p>
+            <p>{subject}</p>
           </div>
-          <div className='content-card-description'>
-              <p>
-								{description}
-              </p>
+          <div className='content-title'>
+            <p style={{fontWeight: 'bold'}}>Secretário:</p>
+            <p>{secretary}</p>
           </div>
+          <div className='content-title'>
+            <p style={{fontWeight: 'bold'}}>Arquivo:</p>
+            <p>{newFileName}</p>
+          </div>
+        </div>
+        <button>
+          <p>Abrir</p>
+          <BsBack />
+        </button>
       </div>
+      <div className='content-card-description'>
+        <p>{description}</p>
+      </div>
+    </div>
   );
 }
 
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import UtilsFunctions from '../../Utils/Utils.functions';
 import { BsBack } from 'react-icons/bs';
 
 import './styles.css';
 
 import Logo from '../../Assets/logo.png';
 
-function Sidebar() {
+function Sidebar({facet_subject, facet_secretary, facet_speaker}) {
+  
+  const newFacetSubject = UtilsFunctions.formatFacets(facet_subject);
+  const newFacetSecretary = UtilsFunctions.formatFacets(facet_secretary);
+  const newFacetSpeaker = UtilsFunctions.formatFacets(facet_speaker);
+
   return (
-      <div className='container-sidebar'>
-          <div className='content-logo'>
-              <img src={Logo} alt="Logo" />
-              <p>Busca de Documentos</p>
-          </div>
-          <div className='line'></div>
-          <div className='content-sidebar-buttons'>
-              <button>
-                  <p>Assunto</p>
-                  <BsBack />
-              </button>
-              <button>
-                  <p>Secretário</p>
-                  <BsBack />
-              </button>
-              <button>
-                  <p>Palestrante</p>
-                  <BsBack />
-              </button>
-          </div>
+    <div className='container-sidebar'>
+      <div className='content-logo'>
+        <img src={Logo} alt="Logo" />
+        <p>Busca de Documentos</p>
       </div>
+      <div className='line'></div>
+      <div className='content-sidebar-buttons'>
+        <button>
+          <p>Assunto ({newFacetSubject.length})</p>
+          <BsBack />
+        </button>
+        <button>
+          <p>Secretário ({newFacetSecretary.length})</p>
+          <BsBack />
+        </button>
+        <button>
+          <p>Palestrante ({newFacetSpeaker.length})</p>
+          <BsBack />
+        </button>
+      </div>
+    </div>
   );
 }
 
