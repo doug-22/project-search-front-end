eb8c868f5bedecab5253fc889d4b0386b4534bd4
Douglas <douglas.aguiar410@gmail.com>
2022-05-26 15:59:08 -0300

    feat: creating utils functions

---
+++ src/Utils/Utils.functions.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line
+export default {
+    formatFacets: (list) => {
+        let array_facet = [];
+        for(let i = 1; i <= list.length;) {
+            if(list[i] === 0) {
+              i += 2;
+            }else {
+              let newSubject = {
+                title: list[i-1],
+                amount: list[i]
+              }
+              array_facet.push(newSubject)
+              i += 2
+            } 
+          }
+        return array_facet;
+    }
+}
