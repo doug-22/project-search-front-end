3036ab75c705d6ad84ecdcd23ff597370d42e49f
Douglas <douglas.aguiar410@gmail.com>
2022-05-31 14:52:46 -0300

    refactor: correcting css for correct functioning of the modal

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UtilsFunctions from '../../Utils/Utils.functions';
 import { BsBack } from 'react-icons/bs';
 
 import './styles.css';
 
 import Logo from '../../Assets/logo.png';
+import ModalFacet from '../ModalFacet';
 
 function Sidebar({facet_subject, facet_secretary, facet_speaker}) {
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalData, setModalData] = useState();
+
+  const handleSetModal = (data) => {
+    setModalData(data);
+    setModalVisible(true);
+  }
   
-  const newFacetSubject = UtilsFunctions.formatFacets(facet_subject);
-  const newFacetSecretary = UtilsFunctions.formatFacets(facet_secretary);
-  const newFacetSpeaker = UtilsFunctions.formatFacets(facet_speaker);
+  const newFacetSubject = UtilsFunctions.formatFacets('Assunto', facet_subject);
+  const newFacetSecretary = UtilsFunctions.formatFacets('Secretário', facet_secretary);
+  const newFacetSpeaker = UtilsFunctions.formatFacets('Palestrante', facet_speaker);
 
   return (
     <div className='container-sidebar'>
@@ -20,19 +29,20 @@ function Sidebar({facet_subject, facet_secretary, facet_speaker}) {
       </div>
       <div className='line'></div>
       <div className='content-sidebar-buttons'>
-        <button>
-          <p>Assunto ({newFacetSubject.length})</p>
+        <button onClick={() => handleSetModal(newFacetSubject)}>
+          <p>Assunto ({newFacetSubject.data.length})</p>
           <BsBack />
         </button>
-        <button>
-          <p>Secretário ({newFacetSecretary.length})</p>
+        <button onClick={() => handleSetModal(newFacetSecretary)}>
+          <p>Secretário ({newFacetSecretary.data.length})</p>
           <BsBack />
         </button>
-        <button>
-          <p>Palestrante ({newFacetSpeaker.length})</p>
+        <button onClick={() => handleSetModal(newFacetSpeaker)}>
+          <p>Palestrante ({newFacetSpeaker.data.length})</p>
           <BsBack />
         </button>
       </div>
+      {modalVisible ? <ModalFacet onclose={() => setModalVisible(false)} facetData={modalData} /> : null}
     </div>
   );
 }
