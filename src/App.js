95c970fcf9c7af7661c0ca45d556f68ff35b55d9
Douglas <douglas.aguiar410@gmail.com>
2022-06-02 16:30:59 -0300

    feat: adding context api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './Pages/Home';
 import './App.css';
 
-
+import Context from './Contexts/context';
 
 function App() {
+
+  const initialState = {
+    data: undefined,
+    facets: undefined,
+    filters: '',
+    queryString: '',
+    search: {
+      type: '',
+      term: '',
+      toggleDate: false,
+      initialDate: '',
+      finalDate: ''
+  }
+}
+
+  const [context, setContext] = useState(initialState)
+
   return (
+    <Context.Provider value={[context, setContext]}>
       <Home />
+    </Context.Provider>
   );
 }
 
