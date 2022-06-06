1f7bd6616c2b79ebadfdeedc8a51ba882d6d5e3b
Douglas <douglas.aguiar410@gmail.com>
2022-06-06 09:40:14 -0300

    feat: adding query string for highlighted search

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -8,31 +8,31 @@ const Api = Axios.create({
 export default {
   getSearch: async (term) => {
     if(term === undefined) {
-      let response = await Api.get('/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=*:*&fl=*&wt=json');
+      let response = await Api.get('/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=*:*&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json');
       return response;
     }else {
-      let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=${term}&fl=*&wt=json`);
+      let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=${term}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
       return response;
     } 
   },
   getSpecificSearch: async (type, term) => {
-    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=${type}:${term}&fl=*&wt=json`)
+    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=${type}:${term}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`)
     return response;
   },
   getSpecificSearchTimeRange: async (type, term, initialDate, finalDate) => {
-    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&fq=fechaResolucion:[${initialDate}T05:00:00Z%20TO%20${finalDate}T05:00:00Z]&q=${type}:${term}&fl=*&wt=json`);
+    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&fq=fechaResolucion:[${initialDate}T05:00:00Z%20TO%20${finalDate}T05:00:00Z]&q=${type}:${term}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
     return response;
   },
   getSearchTimeRange: async (term, initialDate, finalDate) => {
-    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&fq=fechaResolucion:[${initialDate}T05:00:00Z%20TO%20${finalDate}T05:00:00Z]&q=${term}&fl=*&wt=json`);
+    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&fq=fechaResolucion:[${initialDate}T05:00:00Z%20TO%20${finalDate}T05:00:00Z]&q=${term}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
     return response;
   },
   getFacetSearch: async (queryString) => {
     if(queryString.includes('&q=')){
-      let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on${queryString}&fl=*&wt=json`);
+      let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on${queryString}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
       return response;
     }
-    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=*:*${queryString}&fl=*&wt=json`);
+    let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=*:*${queryString}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
     return response;
   }
 };
