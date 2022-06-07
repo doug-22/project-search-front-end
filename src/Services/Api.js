89b66566bc764b28cce4d68c2bb4b38b853258e0
Douglas <douglas.aguiar410@gmail.com>
2022-06-07 18:25:58 -0300

    feat: implementing pagination component in the code

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -9,6 +9,9 @@ export default {
   getSearchResults: async (queryString) => {
     if(queryString.includes('&q=')){
       let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on${queryString}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
+      if(response.data.response.numFound === 0) {
+        response = await Api.get(`/spell?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on${queryString}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`)
+      }
       return response;
     }
     let response = await Api.get(`/select?facet.field=tipoAsunto_facet&facet.field=secretario_facet&facet.field=ponente_facet&facet=on&q=*:*${queryString}&fl=*&hl.fl=data_file&hl.snippets=10&hl=on&wt=json`);
